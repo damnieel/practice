c1fb28b87f5562e37d93c14c090ac151d17dcafa
xiaohui <331712034@qq.com>
2019-11-14 15:42:53 +0800

    new file

---
+++ pct/pct-springboot/src/test/java/com/cvc/springboot/MySignal.java
@@ -0,0 +1,47 @@
+package com.cvc.springboot;
+/**
+ * 线程通信的方式：共享变量
+ * 线程间通信可以通过发送信号，发送信号的一个简单方式是在共享对象的变量里设置信号值。
+ * 线程A在一个同步块里设置boolean型成员变量hasDataToProcess为true，
+ * 线程B也在同步块里读取hasDataToProcess这个成员变量。
+ * 这个简单的例子使用了一个持有信号的对象，并提供了set和get方法：
+ */
+public class MySignal {
+	  //共享的变量
+    private boolean hasDataToProcess = false;
+
+    //取值
+    public boolean getHasDataToProcess() {
+        return hasDataToProcess;
+    }
+    //存值
+    public void setHasDataToProcess(boolean hasDataToProcess) {
+        this.hasDataToProcess = hasDataToProcess;
+    }
+
+    public static void main(String[] args) {
+        //同一个对象
+        final MySignal my = new MySignal();
+        //线程 1 设置 hasDataToProcess 值为 true
+        final Thread t1 = new Thread(new Runnable() {
+            public void run() {
+                my.setHasDataToProcess(true);
+            }
+        });
+        t1.start();
+        //线程 2 取这个值 hasDataToProcess
+        Thread t2 = new Thread(new Runnable() {
+            public void run() {
+                try {
+                    //等待线程 1 完成然后取值
+                    t1.join();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                my.getHasDataToProcess();
+                System.out.println("t1 改变以后的值：" + my.getHasDataToProcess());
+            }
+        });
+        t2.start();
+    }
+}
---
+++ pct/pct-springboot/src/test/java/com/cvc/springboot/ThreadCommunication.java
@@ -0,0 +1,43 @@
+package com.cvc.springboot;
+
+public class ThreadCommunication {
+	private static int num;// 定义一个变量作为数据8.
+
+	public static void main(String[] args) throws InterruptedException {
+
+		Thread threadA = new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				try {
+					// 模拟耗时操作之后初始化变量 num
+					Thread.sleep(1000);
+					num = 1;
+
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		});
+		Thread threadB = new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				System.out.println(Thread.currentThread().getName() + "获取到 num 的值为：" + num);
+			}
+		});
+		Thread threadC = new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				System.out.println(Thread.currentThread().getName() + "获取到 num 的值为：" + num);
+			}
+		});
+		// 同时开启 3 个线程
+		threadA.start();
+		threadA.join();
+		threadB.start();
+		threadC.start();
+		
+	}
+}
